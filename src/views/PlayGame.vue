93c5e9054240d4b2d74917e5fb638926e8cf9db5
Sergei <sergei.jagur@gmail.com>
2022-05-02 23:36:07 +0300

    SERGEI new functions adde

--- src/views/PlayGame.vue
+++ src/views/PlayGame.vue
@@ -1,45 +1,82 @@
 <template>
   <div >
-    <h1>testime local storaget</h1>
-      {{userId}}
-    <span>Valitud roll {{selected}}</span>
-    <div>
-      <select v-model="selected" >
-        <option disabled value="">Vali roll</option>
-        <option v-for="option in options" :value="option.roleId">{{option.roleName}}</option>
-      </select>
-      <br>
-    </div>
+
+
+
+  <div >
+    <button v-on:click="getAllHorsesList" type="button" class="btn btn-primary btn-lg">Show all result</button>
     <br>
     <br>
-    <button v-on:click="deleteDataToLocalStorage">Kustuta andmed local storage alt</button>
+    <br>
+
+    {{raceHorses}}
   </div>
+
+  <div >
+    <table class="table table-hover">
+      <thead>
+      <tr >
+        <th scope="col">Horse name</th>
+        <th scope="col">Horse color</th>
+        <th scope="col"></th>
+
+      </tr>
+      </thead>
+      <tbody>
+
+      <tr v-for="horse in horseList" >
+        <td>{{ horse.name }}</td>
+        <td>{{ horse.color}}</td>
+        <td>
+          <input v-model="raceHorses" type="checkbox" name="" id="" v-bind:value="horse.id">
+        </td>
+
+        <!--            <td><button v-on:click.once="horsesId.push(customer.id)" type="button" class="btn btn-secondary btn-lg">Lisa</button></td>-->
+        <!--          <td><button v-on:click ="horsesId.splice(horsesId.indexOf(customer), 1)" type="button" class="btn btn-secondary btn-lg">Eemalda</button></td>-->
+      </tr>
+      </tbody>
+    </table>
+
+    <!--        <td><button v-on:click="saveDataToSessionStorage" type="button" class="btn btn-secondary btn-lg">edasta</button></td>-->
+
+    <br>
+    <br>
+    <button type="button" name="btn" class="btn btn-secondary btn-sm m-3" >Back</button>
+  </div>
+
+  </div>
+
 </template>
 
 <script>
+
 export default {
   name: "About",
   data: function () {
     return {
-      options: {},
+      horseList: {},
+      raceHorses: [],
       selected: "",
       userId: sessionStorage.getItem('userId'),
+
     }
   },
-  beforeMount() {
-    // todo: taasta roleSelected (string)
-    // todo: taasta roleOptions (taasta objekt JSON'ist)
-    this.selected = localStorage.getItem('roleSelected')
-    this.options = JSON.parse(localStorage.getItem('roleOptions'))
-  },
+
   methods: {
-    deleteDataToLocalStorage: function () {
-      // todo: kustuta roleSelected
-      // todo: kustuta roleOptions
-      localStorage.removeItem('roleSelected')
-      localStorage.removeItem('roleOptions')
-    }
-  }
+
+
+
+
+  getAllHorsesList: function () {
+    this.$http.get('/horse/all-horses')
+        .then(response => {
+          this.horseList = response.data
+          console.log(response.data)
+        })
+        .catch(error => alert(error.response.data.title + ". " + error.response.data.detail))
+  },
+
 
 }
+ }
 </script>
